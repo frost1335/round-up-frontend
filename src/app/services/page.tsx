34a40ee71bc8fe1d6e233faf9d98684d658d8489
frost1335 <dilrozbek.developer@gmail.com>
2025-07-24 09:31:42 +0500

    add: services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,26 +1,35 @@
 import BottomMenu from '@/components/BottomMenu/page'
 import Link from 'next/link'
 import React from 'react'
+import { DepositIcon } from './icons'
+import Image from 'next/image'
 
 function page() {
     const list = [
-        { name: "Round-Up", link: "/services/round-up" },
-        { name: "Кредит", link: "/services/credit" },
-        { name: "Payme Plus", link: "/services/payme-plus" },
-        { name: "Отсрочка платежа", link: "/services/installment" },
-        { name: "Payme Tickets", link: "/services/payme-tickets" },
-        { name: "Благотворительность", link: "/services/thank-you" },
+        { name: "Round-Up", link: "/services/round-up", icon: DepositIcon },
+        { name: "Кредит", link: "/services/credit", icon: DepositIcon },
+        { name: "Payme Plus", link: "/services/payme-plus", icon: DepositIcon },
+        { name: "Отсрочка платежа", link: "/services/installment", icon: DepositIcon },
+        { name: "Payme Tickets", link: "/services/payme-tickets", icon: DepositIcon },
+        {
+            name: "Благотворительность", link: "/services/thank-you", icon: DepositIcon
+        },
     ]
 
 
     return (
         <>
             <div className='h-full bg-[#F6F8F9]'>
-                <h2 className='px-4 py-4 text-lg font-semibold text-neutral-600'>Services</h2>
-                <ul className='px-4 py-2 space-y-2'>
+                <h2 className='px-4 py-4 pt-6 text-lg font-semibold text-neutral-600'>Services</h2>
+                <ul className='px-4 py-2 space-y-2.5'>
                     {list.map((item, index) => (
                         <li key={index}>
-                            <Link className='flex w-full px-5 py-4 rounded-xl shadow-md text-xs text-neutral-600' href={item.link}>{item.name}</Link>
+                            <Link className='flex items-center w-full px-4 py-2 gap-4 rounded-xl text-xs text-neutral-600 shadow-[2px_2px_6px_#ccc]' href={item.link}>
+                                <span>
+                                    <Image src={item.icon} width={42} height={42} alt='Service Icon' />
+                                </span>
+                                {item.name}
+                            </Link>
                         </li>
                     ))}
                 </ul>

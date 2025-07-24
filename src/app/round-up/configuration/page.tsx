d676f721b96925d191579cb7f84cae46d6465735
Dilrozbek Rakhimov <dilrozbek.developer@gmail.com>
2025-07-24 18:47:20 +0500

    start main page

--- src/app/round-up/configuration/page.tsx
+++ src/app/round-up/configuration/page.tsx
@@ -19,12 +19,17 @@ function page() {
             </header>
 
             <main className='flex flex-1 flex-col items-center justify-center w-full px-3 py-3 shadow-[0_-40px_50px_15px_rgba(0,0,0,0.1)] bg-[#F6F8F9] shadow-[#F6F8F9]'>
-                <Slider className='bg-[#2ACED0] text-[#2ACED0]' defaultValue={[5000]} max={10000} step={1000} color='#2ACED0' />
+                <ul className='flex items-center justify-between w-full mb-6 text-neutral-600 font-semibold text-sm'>
+                    <li>1000</li>
+                    <li>5000</li>
+                    <li>10000</li>
+                </ul>
+                <Slider className='bg-[#2ACED0] text-[#2ACED0]' defaultValue={[5000]} max={10000} step={5000} color='#2ACED0' />
             </main>
 
             <footer className='bg-[#F6F8F9] w-full px-3 py-7 flex flex-col'>
                 <div className='px-2 mb-6'>
-                    <Checkbox className='border-[#2ACED0] border-2 text-lg' id="round-up" aria-label='Enable Round-Up' />
+                    <Checkbox className='border-[#2ACED0] checked:bg-[#2ACED0] border-2 text-lg' id="round-up" aria-label='Enable Round-Up' />
                     <label htmlFor="round-up" className='text-sm font-medium ml-2 text-neutral-600'>I accept the terms of `TBC Deposit`</label>
                 </div>
                 <Button variant='default' size={'lg'} color='#2ACED0' className='w-full px-5 py-6 bg-[#2ACED0]'>Primary Button</Button>
